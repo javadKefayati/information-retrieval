dccb1fc952158584776f5b088b2626dc2ac4edd1
javad <javad936807@gmail.com>
2023-04-21 22:14:32 +0330

    init ex1_part2

---
+++ IR_T1_practical/code2/main.py
@@ -0,0 +1,54 @@
+import os
+from whoosh.index import create_in
+from whoosh.fields import *
+from whoosh.qparser import QueryParser
+from whoosh.analysis import StemmingAnalyzer
+from whoosh import query
+from whoosh.analysis import StopFilter, StemmingAnalyzer
+from whoosh.lang.porter import stem
+from whoosh.lang.stopwords import stoplists
+import glob
+
+def create_index_directory(path="" , name="index"):
+  if not os.path.exists(path+"index"):
+      os.mkdir(path+"index")
+  return  create_in("index", schema)
+  
+def create_schema():
+  stopwords = frozenset(stoplists["en"])
+
+  analyzer = StemmingAnalyzer(stemfn=stem, stoplist=stopwords) | StopFilter(stoplist=stopwords)
+
+  return  Schema(title=TEXT(stored=True), path=ID(stored=True), content=TEXT(analyzer=analyzer))
+
+
+def indexing_docs(docs_addresses):
+  writer = ix.writer()
+  
+  for i, file in enumerate(files):
+      with open(file, 'r') as f:
+        contents = f.read()
+
+      writer.add_document(title=file, path= file , content=contents)
+
+  writer.commit()
+
+
+def searcher(boolean_query):
+  #for create parser and return docs number 
+  with ix.searcher() as searcher:
+      parser = QueryParser("content",  termclass=query.Variations ,schema=schema)
+      q = parser.parse(boolean_query)
+      results = searcher.search(q)
+      for i , r in  enumerate(results):
+        title = r["title"]
+        print(f"score: {i+1}          -----       doc name: {title}")
+    
+global schema , ix
+files = glob.glob('./docs/*.txt')
+schema = create_schema()
+ix = create_index_directory()
+indexing_docs(files)
+
+query_input = input("Enter query : ")
+searcher(query_input)
