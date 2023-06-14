9b88213c51352a122cf54f3bc1387957bc8c56ed
javad <javad936807@gmail.com>
2023-06-14 11:59:43 +0330

    feat: define query with cos algo

--- IR_T3_practical/searcher.py
+++ IR_T3_practical/searcher.py
@@ -9,6 +9,85 @@ from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.naive_bayes import MultinomialNB
 from typing import Tuple,List
 import json
+
+class CosineSimilarity:
+    def __init__(self, documents: List[Tuple[str, str]]) -> None:
+        """ This function create list of vector for analyze content and scoring
+
+        Args:
+            documents (List[Tuple[str, str]]): List of document 
+        """
+        
+        self.doc_freqs : Dict[str, int]  = Counter()
+        self.create_doc_frequency(documents)
+
+        self.idf: Dict[str, float]  = {}
+        self.create_inverse_term_frequency(documents)
+
+        self.doc_vectors:Dict[int,Tuple[str,List[float]]] = {}
+        self.create_document_vectors(documents)
+
+    def create_doc_frequency(self, documents : List[Tuple[str, str]]) -> None:
+        """ This function create list of document frequency that contain word and number of
+            repeat in all documents
+
+        Args:
+            documents (List[Tuple[str, str]]): list of document 
+        """
+        for ـ, doc in documents:
+            self.doc_freqs.update(Counter(doc.split()))
+
+    def create_inverse_term_frequency(self, documents : List[Tuple[str, str]]) -> None:
+        """ Create inverse term frequency with this formula 
+            idf for specified word = log(length of all documents / number of repeats in all docs)
+
+        Args:
+            documents (List[Tuple[str, str]]): List of document 
+        """
+        num_docs = len(documents)
+        for word, freq in self.doc_freqs.items():
+            self.idf[word] = math.log(num_docs / freq)
+
+    def create_document_vectors(self, documents: List[Tuple[str, str]]) -> None:
+        """ This functions create list of tuples that contain title and
+            inverse term frequency each word * term frequency of each word
+
+        Args:
+            documents (List[Tuple[str, str]]): List of document 
+        """
+        for i, (title, doc) in enumerate(documents):
+            tf = Counter(doc.split())
+            vec = [tf[w] * self.idf[w] for w in self.doc_freqs.keys()]
+            self.doc_vectors[i] = (title, vec)
+        
+            
+
+    def cosine_similarity(self, query :str ="query") -> List[Tuple[str, float]]:
+        """ This func create list of the approximate amount query with each document
+
+        Args:
+            query (str, optional): A string that the user enters to measure the similarity of the text with other texts. Defaults to "query".
+
+        Returns:
+            List[Tuple[str, float]]: List of the approximate amount query with each document
+        """
+        # Removed stop words and finds the root of the rest
+        tf: Dict[str, int] = Counter(query.split())
+        q_vec:List[float] = [tf[w] * self.idf[w] if w in tf else 0 for w in self.doc_freqs.keys()]
+        sims:List[Tuple[str, float]] = []
+        for _, (title, d_vec) in self.doc_vectors.items():
+            dot_product:float = sum([a*b for a,b in zip(d_vec, q_vec)])
+            norm_d:float = math.sqrt(sum([a*a for a in d_vec]))
+            norm_q:float = math.sqrt(sum([a*a for a in q_vec]))
+            try:
+                sim:float = dot_product / (norm_d * norm_q)
+            except ZeroDivisionError:
+                sim:float = dot_product / ((norm_d * norm_q)+0.0001)
+            sims.append((title, sim))
+
+        return sims
+    
+
 class Searcher:
     header_tags = ["h1", "h2", "h3", "h4", "h5", "h6","title"]
     simple_tags = ["p","span","div","button"]
@@ -75,7 +154,7 @@ class Searcher:
             List[Tuple[str,float]]: A list contain score and document number
         """
         return heapq.nlargest(n_first,self.scores_details, key=lambda x: x[1])
-    def query(self, query:str= "query")->None:
+    def query_with_bayes_algo(self, query:str= "query")->None:
         """calculating rank
 
         Args:
@@ -96,7 +175,12 @@ class Searcher:
         # print(new_vec)
         scores = clf.predict_proba(new_vec)
         self.scores_details = tuple(zip(self.files_name, scores[0]))
-
+    
+    def query_with_cos_algo(self,query=""):
+        self.cosineSimilarity = CosineSimilarity(self.information)
+        query = self.preprocessed_text(query)
+        self.scores_details = self.cosineSimilarity.cosine_similarity(query)
+        
     def print_results(self, n_first:int = 10)-> None:
         """ Print all ranked score 
 
